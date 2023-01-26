f834669187a8cd2bddb51976063a47a9b4ce7994
Logan <lgntravis@gmail.com>
2023-01-26 15:01:21 -0700

    init

---
+++ powersof2.py
@@ -0,0 +1,5 @@
+data = input().split()
+
+pow_len = len(str(2 ** int(data[1])))
+num_length = len(data[0])
+
---
+++ powersof2_easy.py
@@ -0,0 +1,10 @@
+num = input().split()
+
+count = 0
+pow = (2 ** int(num[1]))
+strpow = str(pow)
+for i in range(pow, int(num[0])+1):
+    if strpow in str(i):
+        count += 1
+
+print(count) 
